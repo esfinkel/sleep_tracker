7eb34bf58222606c87c70e6e3166efe94fe39175
esfinkel <finkelelisabeth@gmail.com>
2022-07-24 23:01:54 -0400

    sleep grouping algorithm

--- frontend/src/SleepChart.jsx
+++ frontend/src/SleepChart.jsx
@@ -3,14 +3,61 @@ import {
     AreaChart, Area, XAxis, YAxis, CartesianGrid, Tooltip,
   } from 'recharts';
 
+const getDateString = (date) => {  // a Date object
+  return (date.getMonth()+1).toString() + "/" + date.getDate().toString() + "/" + date.getFullYear().toString()
+}
+
+const addTimeDelta = (date, deltaInMinutes) => {
+  return new Date(date.getTime() + deltaInMinutes*60000);
+};
+
+const breakUpSleep = (sleep) => {
+    let res = {};
+    let counter = new Date(sleep.start);
+    let cutoff = 17 * 60;  // 5pm in minutes
+    while (counter < new Date(sleep.end)) {
+      // Has sleep for the morning of DATE
+      let dateOwnedBy = counter.getHours() < 17 ? counter : addTimeDelta(counter, cutoff); 
+      let dateString = getDateString(dateOwnedBy);
+      if (res[dateString] === undefined) {
+        res[dateString] = 0;
+      }
+      res[dateString] += 1;
+      counter = addTimeDelta(counter, 1); // add 1 minute (i.e. 60 * 1000 ms)
+    }
+    console.log(res);
+    return res;
+};
+
 export default ({sleeps}) => {
-    const rawSleepTimes = sleeps.map(x => ((new Date(x.end) - new Date(x.start)) / (1000*3600)));
-    const sleepTimes = rawSleepTimes.map(x => x.toFixed(1));
-    const data = sleeps.map(s => {return {"date":(new Date(s.end).getMonth()+1).toString()
-    + "/" + new Date(s.end).getDate().toString() + "/" + new Date(s.end).getFullYear().toString(),
-    "blank":"",
-    "amt":((new Date(s.end) - new Date(s.start)) / (1000*3600)).toFixed(2)}});
-    
+    let timeCounter = {};
+    for (let sleep of sleeps) {
+      let res = breakUpSleep(sleep);
+      for (let [dateString, duration] of Object.entries(res)) {
+        if (timeCounter[dateString] === undefined) {
+          timeCounter[dateString] = 0;
+        };
+        timeCounter[dateString] += duration;
+      }
+    };
+    // console.log(timeCounter);
+
+    let data = [];
+    for (let [dateString, duration] of Object.entries(timeCounter)) {
+      data.push(
+        {
+          "date": dateString,
+          "blank":"",
+          "amt":(duration/60).toFixed(1),
+        }
+      );
+    };
+
+    let sleepTimes = [];
+    for (let [_, duration] of Object.entries(timeCounter)) {
+      sleepTimes.push(duration / 60);
+    }
+
     const CustomTooltip = ({ payload, label, active }) => {
         if (active) {
             return (
@@ -24,7 +71,7 @@ export default ({sleeps}) => {
       }
     
     let tickArray = [];
-    for (let i = 0; i < Math.max(...rawSleepTimes); i += 2) {
+    for (let i = 0; i < Math.max(...sleepTimes); i += 2) {
       tickArray.push(i);
     }
 
